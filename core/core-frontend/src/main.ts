cf143ac6c0e634d7b0cd05a7a600763cdcf4c783
hello-github-ui <util.you.come@gmail.com>
2025-05-14 23:16:44 +0800

    dev分支-汇总表分组表头名乱码处理-2-还未修改好

--- core/core-frontend/src/main.ts
+++ core/core-frontend/src/main.ts
@@ -6,8 +6,5 @@ import directive from './directive'
 import Svg from '@/assets/svg'
 import ResetView from '@/utils/reset-view'
 
-// 导入 S2 补丁
-import './views/chart/components/js/antv_s2_patch.js'
-
 // 初始化 Pinia Store
 setupStore(app) 
